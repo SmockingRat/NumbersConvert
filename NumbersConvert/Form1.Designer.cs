b6dc845c62ccf2588cae3fa0d1ec71fac9eedb2d
SmokingRat <halfbrikbarry@gmail.com>
2022-03-07 00:12:29 +0300

    Добавьте файлы проекта.

---
+++ NumbersConvert/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace NumbersConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonForConvert = new System.Windows.Forms.Button();
+            this.LabelForResult = new System.Windows.Forms.Label();
+            this.NumFoConv = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ButtonForConvert
+            // 
+            this.ButtonForConvert.Font = new System.Drawing.Font("Microsoft Tai Le", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ButtonForConvert.Location = new System.Drawing.Point(12, 189);
+            this.ButtonForConvert.Name = "ButtonForConvert";
+            this.ButtonForConvert.Size = new System.Drawing.Size(180, 52);
+            this.ButtonForConvert.TabIndex = 0;
+            this.ButtonForConvert.Text = "Нажмите для конвертации";
+            this.ButtonForConvert.UseVisualStyleBackColor = true;
+            this.ButtonForConvert.Click += new System.EventHandler(this.ButtonForConvert_Click);
+            // 
+            // LabelForResult
+            // 
+            this.LabelForResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LabelForResult.AutoSize = true;
+            this.LabelForResult.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelForResult.Location = new System.Drawing.Point(12, 290);
+            this.LabelForResult.Name = "LabelForResult";
+            this.LabelForResult.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.LabelForResult.Size = new System.Drawing.Size(90, 21);
+            this.LabelForResult.TabIndex = 1;
+            this.LabelForResult.Text = "Результат";
+            this.LabelForResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // NumFoConv
+            // 
+            this.NumFoConv.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NumFoConv.Location = new System.Drawing.Point(12, 122);
+            this.NumFoConv.Name = "NumFoConv";
+            this.NumFoConv.Size = new System.Drawing.Size(428, 26);
+            this.NumFoConv.TabIndex = 2;
+            this.NumFoConv.Text = "Введите число (Не более 999 999 и не менее -999 999)";
+            this.NumFoConv.Click += new System.EventHandler(this.NumFoConv_Click);
+            this.NumFoConv.TextChanged += new System.EventHandler(this.NumFoConv_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.NumFoConv);
+            this.Controls.Add(this.LabelForResult);
+            this.Controls.Add(this.ButtonForConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonForConvert;
+        private System.Windows.Forms.Label LabelForResult;
+        private System.Windows.Forms.TextBox NumFoConv;
+    }
+}
+
---
+++ NumbersConvert/Form1.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumbersConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int GetNum;
+        public string Result;
+
+
+        private void ButtonForConvert_Click(object sender, EventArgs e)
+        {
+            GetNum = Convert.ToInt32(NumFoConv.Text);
+            if (GetNum < 0)
+            {
+                Result += "минус ";
+                GetNum *= -1;
+            }
+            int[] ChargeArray = new int [2];
+            ChargeArray[0]= (GetNum - (GetNum % 1000))/1000;
+            ChargeArray[1]= GetNum % 1000;
+            for (int i = 0; i < 2; i++)
+            {
+                if (GetNum == 0)
+                {
+                    Result += "ноль";
+                    i = 1;
+                }
+
+                if (((ChargeArray[i] - ChargeArray[i] % 100) / 100) != 0)
+                {
+                    
+                    switch ((ChargeArray[i] - ChargeArray[i] % 100) / 100)
+                    {
+                        case 1: 
+                            Result += "сто "; 
+                            break;
+                        
+                        case 2: 
+                            Result += "двести "; 
+                            break;
+                        
+                        case 3: 
+                            Result += "триста "; 
+                            break;
+                        
+                        case 4: 
+                            Result += "четыреста "; 
+                            break;
+                        
+                        case 5: 
+                            Result += "пятьсот "; 
+                            break;
+                        
+                        case 6: 
+                            Result += "шестьсот "; 
+                            break;
+                        
+                        case 7: 
+                            Result += "семьсот "; 
+                            break;
+                        
+                        case 8: 
+                            Result += "восемьсот "; 
+                            break;
+                        
+                        case 9: 
+                            Result += "девятьсот "; 
+                            break;
+
+                    }
+
+                }
+
+                if ((((ChargeArray[i] % 100) - ((ChargeArray[i] % 100) % 10)) / 10) != 1)
+                {
+                    switch ((((ChargeArray[i] % 100) - ((ChargeArray[i] % 100)) % 10) / 10))
+                    {
+                        case 2:
+                            Result += "двадцать ";
+                            break;
+
+                        case 3:
+                            Result += "тридцать ";
+                            break;
+
+                        case 4:
+                            Result += "сорок ";
+                            break;
+
+                        case 5:
+                            Result += "пятьдесят ";
+                            break;
+
+                        case 6:
+                            Result += "шестьдесят ";
+                            break;
+
+                        case 7:
+                            Result += "семьдесят ";
+                            break;
+
+                        case 8:
+                            Result += "восемьдесят ";
+                            break;
+
+                        case 9:
+                            Result += "девяносто ";
+                            break;
+
+
+                    }
+
+                    switch (((ChargeArray[i] % 100) % 10))
+                    {
+                        case 1:
+                            if (i == 0)
+                            {
+                                Result += "одна ";
+                            }
+                            else
+                            {
+                                Result += "один ";
+                            }
+                            break;
+
+                        case 2:
+                            if (i == 0)
+                            {
+                                Result += "две ";
+                            }
+                            else
+                            {
+                                Result += "два ";
+                            }
+                            break;
+
+                        case 3:
+                            Result += "три ";
+                            break;
+
+                        case 4:
+                            Result += "четыре ";
+                            break;
+
+                        case 5:
+                            Result += "пять ";
+                            break;
+
+                        case 6:
+                            Result += "шесть ";
+                            break;
+
+                        case 7:
+                            Result += "семь ";
+                            break;
+
+                        case 8:
+                            Result += "восемь ";
+                            break;
+
+                        case 9:
+                            Result += "девять ";
+                            break;
+                    }
+
+                    if (i == 0)
+                    {
+                        switch (((ChargeArray[i] % 100) % 10))
+                        {
+                            case 1:
+                                Result += "тысяча ";
+                                break;
+
+                            case 2:
+                                Result += "тысячи ";
+                                break;
+
+                            case 3:
+                                Result += "тысячи ";
+                                break;
+
+                            case 4:
+                                Result += "тысячи ";
+                                break;
+
+                            case 5:
+                                Result += "тысяч ";
+                                break;
+
+                            case 6:
+                                Result += "тысяч ";
+                                break;
+
+                            case 7:
+                                Result += "тысяч ";
+                                break;
+
+                            case 8:
+                                Result += "тысяч ";
+                                break;
+
+                            case 9:
+                                Result += "тысяч ";
+                                break;
+                        }
+                    }
+
+
+
+                }
+                else
+                {
+                    switch (((ChargeArray[i] % 100) % 10))
+                    {
+                        case 0:
+                            Result += "десять ";
+                            break;
+
+                        case 1:
+                            Result += "одиннадцать ";
+                            break;
+
+                        case 2:
+                            Result += "двенадцать ";
+                            break;
+
+                        case 3:
+                            Result += "тринадцать ";
+                            break;
+
+                        case 4:
+                            Result += "четырнадцать ";
+                            break;
+
+                        case 5:
+                            Result += "пятнадцать ";
+                            break;
+
+                        case 6:
+                            Result += "шестнадцать ";
+                            break;
+
+                        case 7:
+                            Result += "семнадцать ";
+                            break;
+
+                        case 8:
+                            Result += "восемнадцать ";
+                            break;
+
+                        case 9:
+                            Result += "девятнадцать ";
+                            break; 
+                    }
+                    if(i == 0)
+                    {
+                        Result += "тысяч ";
+                    }
+
+                }
+               
+            }
+            LabelForResult.Text = " ";
+            LabelForResult.Text = Result;
+            Result = ""; 
+
+
+        }
+
+        private void NumFoConv_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NumFoConv_Click(object sender, EventArgs e)
+        {
+            NumFoConv.Text = "";
+        }
+    }
+}
